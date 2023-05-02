b576af8f0fc432dd08b8ee18a64412c602d7021b
star2t <star2thb@gmail.com>
2023-05-02 12:12:29 +0900

    style : Input 컴포넌트 css in js 적용

--- todo-list/src/componrents/Input/index.js
+++ todo-list/src/componrents/Input/index.js
@@ -1,5 +1,6 @@
-import "./index.css"
+// import "./index.css"
 import React, { useState } from "react";
+import styled from "styled-components";
 
 const Input = ({ onAddTodo }) => {
   const [title, setTitle] = useState("");
@@ -17,17 +18,48 @@ const Input = ({ onAddTodo }) => {
   }
 
   return (
-    <form className="input_form" onSubmit={handleFormSubmit}>
-      <input
-        className="inputbox"
-        type="text"
-        placeholder="Todo를 입력하세요"
-        value={title}
-        onChange={handleInputChange}
-      />
-      <button className="submit_button" type="submit">Add Todo</button>
-    </form>
+    <InputForm onSubmit={handleFormSubmit}>
+      <InputBox value={title} onChange={handleInputChange}/>
+      <SubmitButton type="submit">Add Todo</SubmitButton>
+    </InputForm>
   );
 }
+
+const InputForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 800px;
+  padding: 10px;
+  margin: 0 10px;
+`;
+
+const InputBox = styled.input.attrs((props) => ({
+  type: "text", placeholder: "Todo를 입력하세요",
+}))`
+  padding: 10px;
+  margin: 10px;
+  width: 600px;
+  height: 20px;
+`;
+
+const SubmitButton = styled.button`
+  display: block;
+  position: relative;
+  float: left;
+  padding: 10px;
+  margin: 10px;
+  width: 100px;
+  height: 40px;
+  font-weight: 600;
+  text-align: center;
+  color: #fff;
+  background: #b2c9f8;
+  border-color: #8eb0f6;
+  border-radius: 5px;
+  &:hover {
+    background: #8eb0f6;
+  }
+`;
 
 export default Input;
